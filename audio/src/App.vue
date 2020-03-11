2ee913dd3324dff637c8556ce3c91523a03309df
丁家乐 <1430205446@qq.com>
2020-03-11 23:29:09 +0800

    audio

---
+++ audio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+/* import Player from './components/Player'
+export default {
+  components: {
+    Player
+  }
+} */
+</script>
+<style lang="stylus">
+/* body
+  background-color #ffedff
+.app
+  position relative
+  z-index 0
+  display flex
+  justify-content center
+  align-items center
+  width 100%
+  height 100%
+  background-color #ffedff */
+</style>
---
+++ audio/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <player />
+  </div>
+</template>
+
+<script>
+import Player from '../components/Player.vue'
+export default {
+  components: {
+    Player
+  }
+}
+</script>
+
+<style lang="scss">
+.home {
+  top: 100px;
+  position: relative;
+  z-index: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #ffedff;
+}
+</style>
